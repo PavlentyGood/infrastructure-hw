fe9f51237c5514e1cd7bfd86ec8364f051e12f57
pavlentygood <pavlentygood@yandex.ru>
2024-04-13 13:45:41 +0300

    add dependencyUpdates config

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,3 +117,18 @@ tasks.docker.configure {
 tasks.check {
 	finalizedBy(tasks.dependencyUpdates)
 }
+
+tasks.dependencyUpdates {
+	revision = "release"
+
+	fun isNonStable(version: String): Boolean {
+		val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+		val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+		val isStable = stableKeyword || regex.matches(version)
+		return isStable.not()
+	}
+
+	rejectVersionIf {
+		isNonStable(candidate.version)
+	}
+}
